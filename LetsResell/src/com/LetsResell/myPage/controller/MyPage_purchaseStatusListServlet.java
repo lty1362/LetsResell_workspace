31db0dcd2594055eac8a1e5c67d78bb83677cc4f
Song minseon <minseonsong4@gmail.com>
2020-09-20 22:49:12 +0900

    20.09.20(송민선)_myPage
    myPage sideMenubar와 연결

---
+++ LetsResell/src/com/LetsResell/myPage/controller/MyPage_purchaseStatusListServlet.java
@@ -0,0 +1,44 @@
+package com.LetsResell.myPage.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyPage_purchaseStatusListServlet
+ */
+@WebServlet("/purchaseDetail.mp")
+public class MyPage_purchaseStatusListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyPage_purchaseStatusListServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		RequestDispatcher view = request.getRequestDispatcher("views/myPage/myPage_purchaseStatusList.jsp");
+		view.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
