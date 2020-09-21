518a38217c5359678054a1cb4cc3ff6eb21633b0
sunkyung95 <37947951+sunkyung95@users.noreply.github.com>
2020-09-22 00:33:11 +0900

    선경_20200921_v1.3
    저장 실패 팝업 추가

---
+++ LetsResell/src/com/LetsResell/myPage/controller/MyPage_failAlertServlet.java
@@ -0,0 +1,43 @@
+package com.LetsResell.myPage.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyPage_failAlertServlet
+ * 저장 실패 처리하는 Servlet
+ */
+@WebServlet("/fail.my")
+public class MyPage_failAlertServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyPage_failAlertServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher view = request.getRequestDispatcher("views/myPage/myPage_failAlert.jsp");
+		view.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
